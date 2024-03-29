b39077c2023a92b7f60eb852b0450cd05912e7c8
Code Help Tutor <1823890830@qq.com>
2024-03-29 02:15:33 +0000

    WeChat cstutorcs CSCI520 Assignment 1 Simulating a Jello Cube

---
+++ starter-csci520-assign1/showCube.h
@@ -0,0 +1,19 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+/*
+
+  USC/Viterbi/Computer Science
+  "Jello Cube" Assignment 1 starter code
+
+*/
+
+
+#ifndef _SHOWCUBE_H_
+#define _SHOWCUBE_H_
+
+void showCube(struct world * jello);
+
+void showBoundingBox();
+
+#endif
